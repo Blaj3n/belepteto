e26def4cf9ab1b22826e6b4ed4237ffe3044349a
Blaj3n <benceulrich@gmail.com>
2024-09-06 16:04:39 +0200

    6. feladat kész

--- belepteto.py
+++ belepteto.py
@@ -4,7 +4,7 @@ beleptetes = []
 with open("bedat.txt", "r", encoding="utf-8") as file:
     for egysor in file:
         egysor = egysor.strip().split()
-        ora_perc = ''.join(egysor[1]).split(":") # ''.join == hozzáfűzés, azaz, egy üres listát létrehoz, és amit te megadsz a zárójelbe, azt hozzá is teszi azonnyomban, tehát ez egy gyorsított append quasi.
+        ora_perc = ''.join(egysor[1]).split(":")    # ''.join == hozzáfűzés, azaz, egy üres listát létrehoz, és amit te megadsz a zárójelbe, azt hozzá is teszi azonnyomban, tehát ez egy gyorsított append quasi.
         beleptetes.append([str(egysor[0]), str(ora_perc[0]), str(ora_perc[1]), int(egysor[2])])  # ora_perc átalakítás nincs kihatással az egysorra
         # egysor = ['CEFX', '07', '00', 1]
 print(beleptetes)
@@ -48,3 +48,33 @@ if len(konyv_kolcson_lista) < menza_szamlalo:
     print("Többen voltak, mint a menzán. ")
 else:
     print("Nem voltak többen, mint a menzán. ")
+
+print("6. feladat ")
+print("Az érintett tanulók: ")
+# tehát a portás 10:50-kor zárta be a kaput, így ezek a srácok 10:45 - 10:50-ig hagyhatták el a helyszínt, és nem csekkolták le magukat, tehát amikor visszajöttek 10:50 - 11:00 között, akkor ha a 4, 3 lehúzót (menza, könyvtár) nem vesszük figyelembe, akkor a kártyájukon kétszer léptek be, ezeket keressük. 1, 1, 2, 1, 2
+# ötlet: szedjük össze az összes ilyen embernek (16 ember) napi tevékenységét, ne vegyük figyelembe a 3, 4, és ha egymás után kapunk két 1-est, akkor elkaptuk a delikvenst.
+
+tizenegy_lista = []
+for tanulo in beleptetes:
+    if ido(10,50) < ido(int(tanulo[1]), int(tanulo[2])) <= ido(11,00) and tanulo[3] == 1:
+        tizenegy_lista.append(tanulo[0])
+# print(tizenegy_lista)
+
+# megegyezzen az azonosító, valamint a 1, 2 kell -> [1, 2, 1, 2], [1, 1, 2, 1, 2]
+
+belepes_kilepes = []
+for tanulo in tizenegy_lista:
+    belso_lista = [tanulo]
+    for egyelem in beleptetes:
+        if tanulo == egyelem[0] and (egyelem[3] == 1 or egyelem[3] == 2):
+            belso_lista.append(egyelem[3])
+    belepes_kilepes.append(belso_lista)
+    belso_lista = []
+# print(belepes_kilepes)
+
+# Létrehozunk egy üres listát(belepes_kilepes). végigfutunk a 10:50 - 11:00 bejövő diákok azonosítóin ('EQBL') ez esetünkben a tanulo == 'EQBL', ezzel kezdhetünk valamit. minden egyes tanuló azonosítójánál végigfutunk a teljes listán (beleptetes) előtte létrehozunk egy üres listát (belso_lista), ez arra szolgál, hogy ebbe tegyük bele a kilepes, és belépés esemény kódját, majd vizsgálódunk, hogy ha a ( tanulo == nagy listánk (beleptetes) azonositójával ) ÉS ( 1-es, vagy 2-es kód van az egyelemen belül ), akkor beletesszük ebbe a belso_lista-ba. Majd ha végigment ez a beleptetes lista, tehát az összetett ciklusunk második ciklusa (amiben a nagy beleptetes listát futjuk át), ez esetben a belso_lista-nk kész van, appendelhetjuk a külsőbe -> belepes-kilepes.
+# Vizsgáld meg megszámlálással (.count), hogy nem egyenlő 1-es és 2-es van-e a listában -> írjuk ki!
+
+for tanulo in belepes_kilepes:
+    if tanulo.count(1) != tanulo.count(2):
+        print(tanulo[0], end=" ")
